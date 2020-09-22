b85b363f30eeda088a8d3529b2d19f000a143ce9
Ali Hassani <68103095+ali-nsua@users.noreply.github.com>
2020-09-22 17:59:34 +0330

    Create a separate classifier module
    Compatibility with the reference scripts, same as before.

--- torchvision/models/segmentation/_utils.py
+++ torchvision/models/segmentation/_utils.py
@@ -34,37 +34,49 @@ class _SimpleSegmentationModel(nn.Module):
         return result
 
 
+class _DeepLabV3PlusDecoder(nn.Module):
+    
+    def __init__(self, aspp, num_classes, llsize=256):
+        super(_DeepLabV3PlusDecoder, self).__init__()
+        self.aspp = aspp
+        self.low_level_module = nn.Sequential(nn.Conv2d(llsize, 48, 1, bias=False),
+                                              nn.BatchNorm2d(48),
+                                              nn.ReLU())
+        self.final_embedding = nn.Sequential(nn.Conv2d(304, 256, kernel_size=3, stride=1, padding=1, bias=False),
+                                             nn.BatchNorm2d(256),
+                                             nn.ReLU(),
+                                             nn.Dropout(0.5),
+                                             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
+                                             nn.BatchNorm2d(256),
+                                             nn.ReLU(),
+                                             nn.Dropout(0.1),
+                                             nn.Conv2d(256, num_classes, kernel_size=1, stride=1))
+
+    def forward(self, features, input_shape):
+        x, low_level_features = features["out"], features["low_level_features"]
+        low_level_features = self.low_level_module(low_level_features)
+        x = self.aspp(x)
+        x = F.interpolate(x, size=low_level_features.size()[2:], mode='bilinear', align_corners=True)
+        x = torch.cat((x, low_level_features), dim=1)
+        x = self.final_embedding(x)
+        x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=True)
+
+        return x
+
+
+
 class _DeepLabV3PlusModel(nn.Module):
 
     def __init__(self, backbone, aspp, num_classes, llsize=256):
         super(_DeepLabV3PlusModel, self).__init__()
         self.backbone = backbone
-        self.aspp = aspp
-        self.low_level_module = nn.Sequential(nn.Conv2d(llsize, 48, 1, bias=False),
-                                              nn.BatchNorm2d(48),
-                                              nn.ReLU())
-        self.final_emb = nn.Sequential(nn.Conv2d(304, 256, kernel_size=3, stride=1, padding=1, bias=False),
-                                       nn.BatchNorm2d(256),
-                                       nn.ReLU(),
-                                       nn.Dropout(0.5),
-                                       nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
-                                       nn.BatchNorm2d(256),
-                                       nn.ReLU(),
-                                       nn.Dropout(0.1),
-                                       nn.Conv2d(256, num_classes, kernel_size=1, stride=1))
+        self.classifier = _DeepLabV3PlusDecoder(aspp, num_classes, llsize)
 
     def forward(self, x):
         input_shape = x.shape[-2:]
         features = self.backbone(x)
 
         result = OrderedDict()
-        x, low_level_features = features["out"], features["low_level_features"]
-        low_level_features = self.low_level_module(low_level_features)
-        x = self.aspp(x)
-        x = F.interpolate(x, size=low_level_features.size()[2:], mode='bilinear', align_corners=True)
-        x = torch.cat((x, low_level_features), dim=1)
-        x = self.final_emb(x)
-        x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=True)
-        result["out"] = x
+        result["out"] = self.classifier(features, input_shape)
 
         return result
