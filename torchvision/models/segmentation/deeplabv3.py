e5c3d628b8a57c52f5db71dbc43de2cdbfcbf369
Ali Hassani <68103095+ali-nsua@users.noreply.github.com>
2020-09-28 10:30:30 +0330

    Switched to [3x3,256]x2

--- torchvision/models/segmentation/deeplabv3.py
+++ torchvision/models/segmentation/deeplabv3.py
@@ -105,6 +105,9 @@ class DeepLabPlusHead(nn.Module):
         self.project = nn.Sequential(nn.Conv2d(304, 256, kernel_size=3, stride=1, padding=1, bias=False),
                                      nn.BatchNorm2d(256),
                                      nn.ReLU(),
+                                     nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
+                                     nn.BatchNorm2d(256),
+                                     nn.ReLU(),
                                      nn.Dropout(0.1),
                                      nn.Conv2d(256, num_classes, kernel_size=1, stride=1))
 
